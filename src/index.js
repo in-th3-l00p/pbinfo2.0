8eb8356dafa284f67b29a8cad45a996c8bc6122b
in-th3-l00p <indianucode0303@gmail.com>
2022-05-18 12:42:29 +0300

    router creat

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<h1>hello world!</h1>} />
+                <Route path="test" element={<p>test</p>} />
+            </Routes> 
+        </BrowserRouter>
+    )
+}
 
 const root = createRoot(document.getElementById("root"))
-root.render(<h1>Hello world!</h1>)
+root.render(<App />)
